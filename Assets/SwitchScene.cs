450cc551666f090270dc7459b1c42e60a14a0f56
Julia <julia.woodward94@gmail.com>
2017-02-28 21:18:05 -0800

    Added main menu scene and switch scene script for play button

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class SwitchScene : MonoBehaviour
+{
+	public void loadGame()
+	{
+		Application.LoadLevel ("Test"); 
+	}
+
+}
+
